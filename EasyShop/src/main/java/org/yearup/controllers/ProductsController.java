e4d83dd18fe22d5d72ff01efaee9b9c3c2603534
enes4u <enesuzunyearup@gmail.com>
2025-06-25 09:36:27 -0400

    fix bugs

--- EasyShop/src/main/java/org/yearup/controllers/ProductsController.java
+++ EasyShop/src/main/java/org/yearup/controllers/ProductsController.java
@@ -79,9 +79,8 @@ public class ProductsController
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateProduct(@PathVariable int id, @RequestBody Product product)
     {
-        try
-        {
-            productDao.create(product);
+        try {
+            productDao.update(id, product); // bug fixed
         }
         catch(Exception ex)
         {
